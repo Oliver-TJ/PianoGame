edfb889ffb3ffaf851e3b4b85b90c32a8f67a1d4
OliverTaiteJupp <07otaite-jupp@brightoncollege.net>
2021-11-12 14:44:17 +0000

    Page now works

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -11,32 +11,28 @@ import {
 
 const Navbar = () => {
     return (
-    <>
-        <Nav>
-            <NavLogo to="/">
-                Logo
-            </NavLogo>
-            <Bars />
-
-            <NavMenu>
-                <NavLink to="/" activeStyle>
-                    Home
-                </NavLink>
-                <NavLink to="/about" activeStyle>
-                    About
-                </NavLink>
-                <NavLink to="/contact" activeStyle>
-                    Contact
-                </NavLink>
-                <NavLink to="/signin" activeStyle>
-                    Sign In
-                </NavLink>
-                <NavBtn>
-                    <NavBtnLink to="/sign-up">Sign Up</NavBtnLink>
-                </NavBtn>
-            </NavMenu>
-        </Nav>
-    </>
+        <>
+            <Nav>
+                <NavLogo to="./Pages/Home">
+                    Logo
+                </NavLogo>
+                <Bars />
+                <NavMenu>
+                    <NavLink to="./Pages/Home" activeStyle>
+                        Home
+                    </NavLink>
+                    <NavLink to="./Pages/SignIn" activeStyle>
+                        Sign In
+                    </NavLink>
+                    <NavLink to="./Pages/PianoGame" activeStyle>
+                        Piano
+                    </NavLink>
+                    <NavBtn>
+                        <NavBtnLink to="./Pages/SignUp">Sign Up</NavBtnLink>
+                    </NavBtn>
+                </NavMenu>
+            </Nav>
+        </>
     )
 }
 
